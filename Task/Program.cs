c654f9499fdcce715ecabd3618a48fa8daeaaa8c
Roman <kastiel25@yandex.ru>
2022-12-22 14:47:07 +0300

    Код решения задачи

---
+++ Task/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] array1 = new string[5] {"12345", "13", "hello", "world", ":)"};
+string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+Console.WriteLine($"Исходный массив");
+PrintArray(array1);
+Console.WriteLine($"Итоговый массив");
+PrintArray(array2);
